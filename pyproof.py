1b008abf791f55bb88ce1410901a1aa040d1f9b5
John Siratt <John Siratt>
2015-12-15 17:06:53 -0600

    Conditional proofing implemented.

--- pyproof.py
+++ pyproof.py
@@ -571,6 +571,38 @@ class Proof:
 			self.show()
 			return			
 		return result
+		
+	def cp(self, expression):
+		if isinstance(expression, logical):
+			last_entry = self.entries[-1]
+			justification = "Assumption (CP)"
+			new_entry = theorem(expression, justification)
+			new_entry.recursion = last_entry.recursion + ["c"]
+			self.entries.append(new_entry)
+			self.show()
+			return
+		return "INVALID INPUT: expression must be logical"
+		
+	def discharge(self):
+		last_entry = self.entries[-1]
+		if len(last_entry.recursion) == 0:
+			return "INVALID COMMAND: nothing to discharge"
+		if last_entry.recursion[-1] == "c":
+			i = 2
+			while len(self.entries[-i].recursion) == len(last_entry.recursion):
+				i +=1
+			antecedent = self.entries[1-i].expression
+			consequent = last_entry.expression
+			result = conditional(antecedent, consequent)
+			justification = "CP " + str(len(self.entries)-i+1) + " - " + str(len(self.entries))
+			new_entry = theorem(result, justification)
+			new_entry.recursion = last_entry.recursion[0:-1]
+			self.entries.append(new_entry)
+			self.show()
+			return
+		if last_entry.recursion[-1] == "i":
+			return "Not yet implemented"
+		return "INVALID COMMAND: unable to discharge"
 	
 	def show(self):
 		i = 1
